61598a2a350474cbc57a0c4e789df6d6e8396140
lukaszdadura <Sklera123>
2021-05-20 22:08:11 +0200

    wyswietlanie zadan zrobione

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,8 +1,39 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String[] availableOptions = {"add", "remove", "list", "exit"};
+        Path tasks = Paths.get("tasks.csv");
+
+        printOptions(availableOptions);
+
+
+        }
+
+
+
+        public static void printOptions(String[] tab) {
+            System.out.println(ConsoleColors.BLUE + "Please select an option:");
+            System.out.print(ConsoleColors.RESET);
+            for (String opt : tab) {
+                System.out.println(opt);
+            }
+        }
+
+
+
+
+
+
+
+
     }
-}
+
