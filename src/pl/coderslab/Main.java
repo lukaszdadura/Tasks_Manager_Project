ae6c96ca8b906ad5d82db83fd7929e549d3daed5
lukaszdadura <Sklera123>
2021-05-21 12:32:49 +0200

    wszystko dziala, jest prymityw w jednej mainie, ale hurrqa!

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -12,10 +12,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static String[][] tasksLinesElements1;
 
     public static void main(String[] args) {
-
 
 
         Path pathTasks = Paths.get("tasks.csv");
@@ -26,8 +24,8 @@ public class Main {
         //Czytam plik i wpisuje do listy. Do tablicy dwuwymiarowej wpisuje eleemnty przyjmujac za wiersze ilosc linii, a za kolumny Stringi po przecinku w linii
         try {
             List<String> tasksLines = Files.readAllLines(pathTasks);
-           // System.out.println(tasksLines);
-           // System.out.println(tasksLines.get(0).split(",").length);
+            // System.out.println(tasksLines);
+            // System.out.println(tasksLines.get(0).split(",").length);
             tasksLinesElements = new String[tasksLines.size()][tasksLines.get(0).split(",").length];
             for (int i = 0; i < tasksLines.size(); i++) {
                 String[] elements = tasksLines.get(i).split(",");
@@ -35,7 +33,7 @@ public class Main {
                     tasksLinesElements[i][j] = elements[j];
                 }
             }
-         //   System.out.println(Arrays.deepToString(tasksLinesElements));
+            //   System.out.println(Arrays.deepToString(tasksLinesElements));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("There is some problem with file!");
@@ -46,75 +44,74 @@ public class Main {
 
         String[] availableOptions = {"add", "remove", "list", "exit"};
         System.out.println(ConsoleColors.BLUE + "Please select an option:");
-            System.out.print(ConsoleColors.RESET);
-            for (String options : availableOptions) {
-                System.out.println(options);
-            }
+        System.out.print(ConsoleColors.RESET);
+        for (String options : availableOptions) {
+            System.out.println(options);
+        }
 
         Scanner scanner = new Scanner(System.in);
-            while (scanner.hasNextLine()) {
-                String typedOption = scanner.nextLine();
-                switch (typedOption) {
-                    case "add":
-                      //  Scanner scanner1 = new Scanner(System.in);
-                        System.out.println("Please add task description");
-                        String description = scanner.nextLine();
-                        System.out.println("Please add task due date");
-                        String dueDate = scanner.nextLine();
-                        System.out.println("Is your task important: true/false");
-                        String important = scanner.nextLine();
-                        tasksLinesElements = Arrays.copyOf(tasksLinesElements, tasksLinesElements.length+1);
-                        tasksLinesElements[tasksLinesElements.length-1] = new String[3];
-                        tasksLinesElements[tasksLinesElements.length-1][0] = description;
-                        tasksLinesElements[tasksLinesElements.length-1][1] = dueDate;
-                        tasksLinesElements[tasksLinesElements.length-1][2] = important;
-                       // System.out.println(Arrays.deepToString(tasksLinesElements));
-                       // scanner.close();
-                        break;
-                    case "remove":
-                        System.out.println("Please select number to remove");
-                        String numberToRemove = scanner.nextLine();
-                        Integer numberToRemoveChanged = Integer.parseInt(numberToRemove);
-                        while (numberToRemoveChanged < 0) {
-                            System.out.println("Incorrect argument passed. Please give number greater or equal 0 and lower or equal last task number.");
-                            scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String typedOption = scanner.nextLine();
+            switch (typedOption) {
+                case "add":
+                    //  Scanner scanner1 = new Scanner(System.in);
+                    System.out.println("Please add task description");
+                    String description = scanner.nextLine();
+                    System.out.println("Please add task due date");
+                    String dueDate = scanner.nextLine();
+                    System.out.println("Is your task important: true/false");
+                    String important = scanner.nextLine();
+                    tasksLinesElements = Arrays.copyOf(tasksLinesElements, tasksLinesElements.length + 1);
+                    tasksLinesElements[tasksLinesElements.length - 1] = new String[3];
+                    tasksLinesElements[tasksLinesElements.length - 1][0] = description;
+                    tasksLinesElements[tasksLinesElements.length - 1][1] = dueDate;
+                    tasksLinesElements[tasksLinesElements.length - 1][2] = important;
+                    // System.out.println(Arrays.deepToString(tasksLinesElements));
+                    // scanner.close();
+                    break;
+                case "remove":
+                    System.out.println("Please select number to remove");
+                    String numberToRemove = scanner.nextLine();
+                    Integer numberToRemoveChanged = Integer.parseInt(numberToRemove);
+                    while (numberToRemoveChanged < 0) {
+                        System.out.println("Incorrect argument passed. Please give number greater or equal 0 and lower or equal last task number.");
+                        scanner.nextLine();
+                    }
+                    if (numberToRemoveChanged < tasksLinesElements.length) {
+                        tasksLinesElements = ArrayUtils.remove(tasksLinesElements, numberToRemoveChanged);
+                        System.out.println("Value was successfully removed");
+                    }
+                    break;
+                case "list":
+                    for (int i = 0; i < tasksLinesElements.length; i++) {
+                        System.out.print(i + " : ");
+                        for (int j = 0; j < tasksLinesElements[i].length; j++) {
+                            System.out.print(tasksLinesElements[i][j] + " ");
                         }
-                        if (numberToRemoveChanged < tasksLinesElements.length) {
-                            tasksLinesElements = ArrayUtils.remove(tasksLinesElements, numberToRemoveChanged);
-                            System.out.println("Value was successfully removed");
+                        System.out.println();
+                    }
+                    // scanner.close();
+                    break;
+                case "exit":
+                    String[] lines = new String[tasksLinesElements.length];
+                    for (int i = 0; i < tasksLinesElements.length; i++) {
+                        lines[i] = String.join(",", tasksLinesElements[i]);
+                        try {
+                            Files.write(pathTasks, Arrays.asList(lines), StandardOpenOption.TRUNCATE_EXISTING);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            System.out.println("There is some problem with saving file!");
                         }
+                    }
+                        System.out.println(ConsoleColors.RED + "Bye, bye.");
+                        System.exit(0);
                         break;
-                    case "list":
-                        for (int i = 0; i < tasksLinesElements.length; i++) {
-                            System.out.print(i + " : ");
-                            for (int j = 0; j < tasksLinesElements[i].length; j++) {
-                                System.out.print(tasksLinesElements[i][j] + " ");
-                            }
-                            System.out.println();
-                        }
-                       // scanner.close();
-                        break;
-                    case "exit":
-                        String[] lines = new String[tasksLinesElements.length];
-                        for (int i = 0; i < tasksLinesElements.length; i++) {
-                            lines[i] = String.join(",", tasksLinesElements[i]);
-                            try {
-                                Files.write(pathTasks, Arrays.asList(lines), StandardOpenOption.TRUNCATE_EXISTING);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                                System.out.println("There is some problem with saving file!");
-                            }
-                            System.out.println(ConsoleColors.RED + "Bye, bye.");
-                            System.exit(0);
-                        }
-                        break;
-                    default:
-                        System.out.println("Type right option!");
-
-                }
+                default:
+                    System.out.println("Type right option!");
             }
         }
-        }
+    }
+}
 
 
 
